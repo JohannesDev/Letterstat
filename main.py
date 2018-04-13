5f746c282914b1dbb201fd3b3caa1433ef6f0d9b
flofriday <flohacksfriday@gmail.com>
2018-04-13 15:19:59 +0100

    Updated readLetters to read file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygal
 
 def readLetters():
-    text = "Hello"
+    text = ""
+    file = open ("README.md", "r")
+    text = file.read()
+    file.close
+
     list = [0] * 256
     for i in range(len(text)):
         list[ord(text[i])] += 1
     print(list)
-    print("started readLetters")
 
 def showGraph():
     print("started showGraph")
