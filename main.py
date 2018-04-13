b7c8485fd212b7fb895872865fb743ad55cd7599
flofriday <flohacksfriday@gmail.com>
2018-04-13 14:46:11 +0100

    First implementation of readLetters()

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def readLetters():
+    text = "Hello"
+    list = [0] * 256
+    for i in range(len(text)):
+        list[ord(text[i])] += 1
+    print(list)
     print("started readLetters")
+
 def showGraph():
     print("started showGraph")
+
 readLetters()
 showGraph()
