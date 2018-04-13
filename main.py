e44dfd8a5b53f04114a8da22fe65fbf5d19bb359
Johannes <33333384+JohannesDev@users.noreply.github.com>
2018-04-13 15:24:38 +0200

    fixed code that was not working because i had to push before my teammate

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def readLetters():
 def showGraph():
     print("started showGraph")
     chart = pygal.Bar()                                            # Then create a bar graph object#
-    for 
     chart.add('A', [20])  # Add some values
     chart.render_to_file('chart.svg')  # Write the chart in the specified file
 
