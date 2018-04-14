d4000a8cf8ce3c699bc963416219588266917047
Johannes <33333384+JohannesDev@users.noreply.github.com>
2018-04-14 10:39:01 +0200

    added parameters

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import pygal
+import sys, getopt
 
 class Letter(object):
     def __init__(self, symbol, amount):
         self.symbol = symbol
         self.amount = amount
 
-def readArgv():
-    pass
+#loading custom inputs from user
+def readArgv(argv):
+    path = 'README.md'
+    graphsize = 10
+    outputfile = 'chart.svg'
+
+    try:
+        opts, args = getopt.getopt(argv,"p:s:o:",["path=","graphsize=","outputfile="])
+    except getopt.GetoptError:
+        print('main.py -p <path> -s <graphsize> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-p':
+            path = arg
+        elif opt == '-s':
+            graphsize = arg
+        elif opt == '-o':
+            outputfile = arg
+
+    return path,graphsize,outputfile
+
 
 def readLetters(path, graphSize = 10):
     text = ""
@@ -18,15 +38,20 @@ def readLetters(path, graphSize = 10):
     for i in range(len(text)):
         list[ord(text[i])] += 1
 
-def showGraph(maxValues):
+    #test output
+    return [Letter("A", 10),Letter("B", 5),Letter("C", 2)]
+
+def showGraph(maxValues, outputfile):
     print("started showGraph")
     chart = pygal.Bar()
     for i in range(len(maxValues)):
         chart.add(maxValues[i].symbol, maxValues[i].amount)
-    chart.render_to_file('chart.svg')
+    if '.svg' in outputfile:
+        chart.render_to_file(outputfile)
+    elif '.png' in outputfile:
+        #convert svg to png
 
-readArgv()
-readLetters("README.md", 10)
 
-highestLetters=  [Letter("A", 10),Letter("B", 5),Letter("C", 2)]
-showGraph(highestLetters)
+path, graphsize, outputfile = readArgv(sys.argv[1:])
+highestLetters = readLetters(path, graphsize)
+showGraph(highestLetters, outputfile)
